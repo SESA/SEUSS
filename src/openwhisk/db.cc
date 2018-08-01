dc87a167b486d98f2fc00cd2ed676fb7cc02d82a
Jim Cadden <jmcadden@bu.edu>
2018-08-01 10:53:22 -0500

    Add missing header include

--- src/openwhisk/db.cc
+++ src/openwhisk/db.cc
@@ -2,6 +2,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 #include <string>
+#include <iostream>
 #include <stdio.h>
 
 #include <pillowtalk.h>

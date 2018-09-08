5ba19d2bc14c427722cfe124701a9f40a1b671db
Jim Cadden <jmcadden@bu.edu>
2018-09-08 15:11:04 -0500

    Support DEBUG env flag in native build

--- build_native.sh
+++ build_native.sh
@@ -9,4 +9,8 @@ mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 make clean
 # Clear any local cmake state in root dir
+if [ -n "$DEBUG" ]; then 
 cmake -DCMAKE_TOOLCHAIN_FILE=$TC_FILE -DCMAKE_BUILD_TYPE=Debug ../ && make -j VERBOSE=1
+else
+cmake -DCMAKE_TOOLCHAIN_FILE=$TC_FILE -DCMAKE_BUILD_TYPE=Release ../ && make -j VERBOSE=1
+fi

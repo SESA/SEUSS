80ccc527c70863f52dac9dffc5c2d00768fe40bc
Jim Cadden <jmcadden@bu.edu>
2018-09-10 14:22:13 -0500

    Move InvocationSession class into to its own files

---
+++ src/InvocationSession.h
@@ -0,0 +1,63 @@
+//          Copyright Boston University SESA Group 2013 - 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+#ifndef SEUSS_INVOCATION_SESSION_H
+#define SEUSS_INVOCATION_SESSION_H
+
+#include <ebbrt/Future.h>
+#include <ebbrt/native/NetTcpHandler.h>
+
+#include "Seuss.h"
+
+namespace seuss {
+
+class InvocationSession : public ebbrt::TcpHandler {
+public:
+  InvocationSession(ebbrt::NetworkManager::TcpPcb pcb, ActivationRecord ar)
+      : ebbrt::TcpHandler(std::move(pcb)), ar_(ar) { Install(); }
+
+  /* Default handler for when the TCP connection is aborted */ 
+  void Abort();
+
+  /* Default handler for when the TCP connection is closed */ 
+  void Close();
+
+  /* Default handler call for when the TCP connection is established */ 
+  void Connected();
+
+  /* Sends an openwhisk NodeJsAction HTTP request */ 
+  void SendHttpRequest(std::string path, std::string payload);
+
+  /* Handler for receiving data on the TCP connection */ 
+  void Receive(std::unique_ptr<ebbrt::MutIOBuf> b);
+
+  /* Finished the invocation session, pass reponse to Invoker */ 
+  void Finish(std::string Response);
+
+  /* Asyncronous hooks for event handlers */
+  ebbrt::SharedFuture<void> WhenClosed();
+  ebbrt::SharedFuture<void> WhenAborted();
+  ebbrt::SharedFuture<void> WhenConnected();
+  ebbrt::SharedFuture<void> WhenInitialized();
+  ebbrt::SharedFuture<std::string> WhenFinished();
+  ActivationRecord GetActivationRecord(){ return ar_;}
+
+private:
+  /* event hooks */
+  ebbrt::Promise<void> when_aborted_;
+  ebbrt::Promise<void> when_closed_;
+  ebbrt::Promise<void> when_connected_;
+  ebbrt::Promise<void> when_initialized_;
+  ebbrt::Promise<std::string> when_finished_; // Not sure about this
+  /* session members */
+  bool is_connected_{false};
+  bool is_initialized_{false};
+  ActivationRecord ar_;
+  ebbrt::clock::Wall::time_point command_clock_;
+  /* helper methods */
+  std::string http_post_request(std::string path, std::string payload);
+
+}; // end class InvocationSession
+} // end namespace seuss
+#endif 
--- src/SeussInvoker.h
+++ src/SeussInvoker.h
@@ -14,64 +14,15 @@
 #include <ebbrt/Future.h>
 #include <ebbrt/MulticoreEbb.h>
 #include <ebbrt/native/Net.h>
-#include <ebbrt/native/NetTcpHandler.h>
 
 #include "umm/src/Umm.h"
 
 #include "Seuss.h"
+#include "InvocationSession.h"
 
 namespace seuss {
 
 void Init();
-
-class InvocationSession : public ebbrt::TcpHandler {
-public:
-  InvocationSession(ebbrt::NetworkManager::TcpPcb pcb, ActivationRecord ar)
-      : ebbrt::TcpHandler(std::move(pcb)), ar_(ar) { Install(); }
-
-  /* Default handler for when the TCP connection is aborted */ 
-  void Abort();
-
-  /* Default handler for when the TCP connection is closed */ 
-  void Close();
-
-  /* Default handler call for when the TCP connection is established */ 
-  void Connected();
-
-  /* Sends an openwhisk NodeJsAction HTTP request */ 
-  void SendHttpRequest(std::string path, std::string payload);
-
-  /* Handler for receiving data on the TCP connection */ 
-  void Receive(std::unique_ptr<ebbrt::MutIOBuf> b);
-
-  /* Finished the invocation session, pass reponse to Invoker */ 
-  void Finish(std::string Response);
-
-  /* Asyncronous hooks for event handlers */
-  ebbrt::SharedFuture<void> WhenClosed();
-  ebbrt::SharedFuture<void> WhenAborted();
-  ebbrt::SharedFuture<void> WhenConnected();
-  ebbrt::SharedFuture<void> WhenInitialized();
-  ebbrt::SharedFuture<std::string> WhenFinished();
-  
-  ActivationRecord GetActivationRecord(){ return ar_;}
-
-private:
-  /* event hooks */
-  ebbrt::Promise<void> when_aborted_;
-  ebbrt::Promise<void> when_closed_;
-  ebbrt::Promise<void> when_connected_;
-  ebbrt::Promise<void> when_initialized_;
-  ebbrt::Promise<std::string> when_finished_; // Not sure about this one...
-
-  bool is_connected_{false};
-  bool is_initialized_{false};
-
-  ActivationRecord ar_;
-  ebbrt::clock::Wall::time_point command_clock_;
-  /* helper functions */
-  std::string http_post_request(std::string path, std::string payload);
-}; // class InvocationSession
 
 /*  suess::Invoker 
  *  Per-core ebb responsible for initializing the
